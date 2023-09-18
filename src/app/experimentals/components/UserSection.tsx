c5435730595288c605c23ab55c79f4def5ca3350
정경훈 <ruderud0055@gmail.com>
2023-09-18 09:27:21 +0900

    feat: user data fetch

--- src/app/experimentals/components/UserSection.tsx
+++ src/app/experimentals/components/UserSection.tsx
@@ -27,42 +27,42 @@ export const UserSection = () => {
       ...data,
     });
     window.localStorage.setItem('userId', userData.id);
-    // enqueueSnackbar(message, { variant: 'success' });
+    enqueueSnackbar(message, { variant: 'success' });
   };
 
   useEffect(() => {
     setUserId(window.localStorage.getItem('userId'));
   }, []);
 
+  useEffect(() => {
+    if (data !== undefined) {
+      // enqueueSnackbar(`get user data: ${data.userData.userName}${data.userData.nameCode}`, { variant: 'success' });
+      enqueueSnackbar(`${JSON.stringify(data)}`, { variant: 'success' });
+    }
+    console.log(data);
+  }, [data]);
+
   return (
     <div>
-      {data ? (
-        <div className="flex flex-col gap-2">
-          <div>{`UserName: ${data.userData.userName}${data.userData.nameCode}`}</div>
+      <h3 className="text-2xl">USER SETTINGS</h3>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col">
+          <label htmlFor="roomName">User Name</label>
+          <input
+            type="text"
+            className={
+              errors.userName ? 'border-2 border-rose-500 rounded p-1 text-black' : 'border-2 rounded p-1 text-black'
+            }
+            {...register('userName', { required: true })}
+          />
+          {errors.userName && <p className="font-bold text-red-500">User Name is required</p>}
         </div>
-      ) : (
-        <>
-          <div className="text-2xl">USER SETTINGS</div>
-          <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col">
-              <label htmlFor="roomName">User Name</label>
-              <input
-                type="text"
-                className={
-                  errors.userName
-                    ? 'border-2 border-rose-500 rounded p-1 text-black'
-                    : 'border-2 rounded p-1 text-black'
-                }
-                {...register('userName', { required: true })}
-              />
-              {errors.userName && <p className="font-bold text-red-500">User Name is required</p>}
-            </div>
-            <Button type="submit" color="green">
-              Sign Up
-            </Button>
-          </form>
-        </>
-      )}
+        <Button type="submit" color="green">
+          Sign Up
+        </Button>
+      </form>
+
+      {data && <div>{JSON.stringify(data)}</div>}
     </div>
   );
 };
--- src/app/experimentals/page.tsx
+++ src/app/experimentals/page.tsx
@@ -1,53 +1,33 @@
-import { axiosInstance } from '@/api/axiosInstance';
-import { reportError } from '@/utils';
-import { SignalingChannel } from './utils';
 import Minecraft from './components/Mincraft';
 import { RoomList } from './components/RoomList';
 import Providers from './providers';
 import { getRooms } from './hooks/useGetRoomsQuery';
 import getQueryClient from '../getQueryClient';
 import { Hydrate, dehydrate } from '@tanstack/react-query';
-import ssrPrepass from 'react-ssr-prepass';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Suspense } from 'react';
+import { UserSection } from './components/UserSection';
 
 export default async function ExperimentalsPage() {
   const queryClient = getQueryClient();
-
-  try {
-    await queryClient.prefetchQuery(['rooms'], getRooms);
-  } catch (error) {
-    return <div>{JSON.stringify(error)}</div>;
-  }
-
-  // const initialRoomsData = await getRooms();
+  await queryClient.prefetchQuery(['rooms'], getRooms);
   const dehydratedState = dehydrate(queryClient);
-  console.log('dehydratedState', dehydratedState);
 
   return (
     <main className="p-5 flex flex-row gap-10">
-      <Providers>
-        <Hydrate state={dehydratedState}>
-          {/* <ErrorBoundary
-          fallback={<div>someThing wrong</div>}
-          onError={(error, errorInfo) => {
-            // console.log(error, errorInfo);
-            // window.localStorage.removeItem('userId');
-            // window.location.reload();
-          }}
-        >
-          <Suspense fallback={'Load Exist User info'}>
-            <UserSection />
-          </Suspense>
-        </ErrorBoundary> */}
+      <ErrorBoundary fallback={<div>Something wrong...</div>}>
+        <Providers>
+          <Hydrate state={dehydratedState}>
+            <Suspense fallback={<div>Loading...</div>}>
+              <UserSection />
+            </Suspense>
 
-          <ErrorBoundary fallback={<div>Something wrong...</div>}>
             <Suspense fallback={<div>Loading...</div>}>
               <RoomList />
             </Suspense>
-          </ErrorBoundary>
-        </Hydrate>
-      </Providers>
+          </Hydrate>
+        </Providers>
+      </ErrorBoundary>
     </main>
   );
 }
