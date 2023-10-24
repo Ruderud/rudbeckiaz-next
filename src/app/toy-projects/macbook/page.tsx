c355d966d5f6e28f4ec6e280345b2324f9a09fa1
정경훈 <ruderud0055@gmail.com>
2023-10-24 17:47:54 +0900

    feat: toy projects page 추가

--- src/app/toy-projects/macbook/page.tsx
+++ src/app/toy-projects/macbook/page.tsx
@@ -3,7 +3,6 @@ import { Scene } from './components/Scene';
 export default function AboutPage() {
   return (
     <main>
-      {/* <AxisTunnel /> */}
       <div className="absolute z-10 top-0 w-screen h-screen">
         <Scene />
       </div>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -13,6 +13,7 @@ export const TopBar = () => {
       </Link>
       <div className="px-5 flex justify-end items-center font-bold gap-5">
         {/* <Link href="/experimentals">Experimental</Link> */}
+        <Link href="/toy-projects">Toy Projects</Link>
         <Link href="/scrawl">Scrawl</Link>
         <Link href="/about">About</Link>
         <DarkModeButton />
