cb41ef05944d4057a574ff79bdf5790e07be2cee
정경훈 <ruderud0055@gmail.com>
2023-10-25 18:39:23 +0900

    feat: scrawl card 추가"

--- src/app/scrawl/page.tsx
+++ src/app/scrawl/page.tsx
@@ -1,14 +1,59 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const metadata = {
   title: 'Rud - scrawl',
   description: 'Unleash Random Thoughts',
 };
+
+type ScrawlCardProps = {
+  title: string;
+  description: string;
+  href: string;
+  imgUrl?: string;
+};
+
+const ScrawlCard = ({ title, description, href, imgUrl }: ScrawlCardProps) => {
+  return (
+    <Link
+      className="w-[320px] h-[350px] bg-white hover:bg-slate-300 dark:bg-slate-800 dark:hover:bg-slate-600 rounded"
+      href={href}
+    >
+      {imgUrl && (
+        <div className="rounded w-[320px] h-[160px] bg-white flex align-center justify-center">
+          <Image
+            className="rounded"
+            src={imgUrl}
+            alt={`${title}-img`}
+            width={320}
+            height={160}
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+      )}
+      <section className="p-4  shadow-inner">
+        <p className="font-bold truncate pb-2">{title}</p>
+        <p>{description}</p>
+      </section>
+    </Link>
+  );
+};
+
 export default function ScrawlPage() {
   return (
-    <main className="min-w-screen min-h-screen px-4 sm:px-8 md:px-20 lg:px-32 xl:px-40 py-10 flex flex-col">
-      <Link href="/scrawl/when-to-use-cloudflare-worker">When to use Cloudflare Workers?</Link>
-      <Link href="/scrawl/aws-lambda-websocket">aws lambda에서 websocket 사용</Link>
+    <main className="min-w-screen min-h-screen px-4 sm:px-8 md:px-20 lg:px-32 xl:px-40 py-10 flex flex-wrap gap-4">
+      <ScrawlCard
+        title="Cloudflare worker를 언제 사용해야할까??????????????"
+        description="Cloudflare worker를 사용하는 시점에 대해 고민해보고, 정해본 개인적 기준"
+        href="/scrawl/when-to-use-cloudflare-worker"
+        imgUrl="https://rudbeckiaz-main-asset.s3.amazonaws.com/scrawl/when-to-use-cloudflare-worker/cloudflare_workers_logo.jpg"
+      />
+      <ScrawlCard
+        title="Aws lambda에서 websocket 사용해보기"
+        description="서버리스환경에서 websocket을 사용하는 방법"
+        href="/scrawl/aws-lambda-websocket"
+        imgUrl="https://rudbeckiaz-main-asset.s3.amazonaws.com/scrawl/aws-lambda-websocket/ws-chat-app.png"
+      />
     </main>
   );
 }
--- src/app/scrawl/when-to-use-cloudflare-worker/page.tsx
+++ src/app/scrawl/when-to-use-cloudflare-worker/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const metadata = {
@@ -14,6 +15,15 @@ export default function ScrawlPage() {
     <main className="min-w-screen min-h-screen px-4 sm:px-8 md:px-20 lg:px-32 xl:px-40 py-10">
       <h1 className="text-xl font-bold">When to use cloudflare-worker?</h1>
       <br />
+
+      <Image
+        src={
+          'https://rudbeckiaz-main-asset.s3.amazonaws.com/scrawl/when-to-use-cloudflare-worker/cloudflare_workers_logo.jpg'
+        }
+        alt="cloudflare_workers_logo"
+        width={300}
+        height={150}
+      />
 
       <section>
         <h2 className="text-lg font-bold pb-2">cloudflare-worker는?</h2>
