db16b1d60079274e196448b9b3655f813af38396
정경훈 <ruderud0055@gmail.com>
2023-11-23 15:27:39 +0900

    test: 문제부분 페이지 임시이관

--- src/app/experimentals/play/components/Dot.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import tw, { styled } from 'twin.macro';
-
-export const Dot = styled.div(() => [
-  tw`absolute z-[30] top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%]`,
-  tw`w-[10px] h-[10px] rounded border-white border-2`,
-]);
